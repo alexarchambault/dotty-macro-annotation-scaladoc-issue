ae138e0c51494d906b3157dc1b7573b7c01c20ff
Alexandre Archambault <alexandre.archambault@gmail.com>
2023-02-09 18:39:15 +0100

    Initial commit

---
+++ Bar.scala
@@ -0,0 +1,29 @@
+//> using scala "3.3.0-RC2"
+
+// from https://github.com/lampepfl/dotty/blob/3d251d6fd1bb1e98d66760ee50b824861a87ca57/tests/run-macros/annot-simple-fib/Test_2.scala
+
+import scala.annotation.experimental
+
+@experimental
+class Bar:
+  @memoize
+  def fib(n: Int): Int =
+    println(s"compute fib of $n")
+    if n <= 1 then n
+    else fib(n - 1) + fib(n - 2)
+  //> val fibCache$macro$1: mutable.Map[Int, Int] = mutable.Map.empty[Int, Int]
+  //> @memoize def fib(n: Int): Int =
+  //>   if fibCache$macro$1.contains(n) then fibCache$macro$1(n)
+  //>   else
+  //>     val res: Int =
+  //>       println(s"compute fib of $n")
+  //>       if n <= 1 then n
+  //>       else fib(n - 1) + fib(n - 2)
+  //>     fibCache$macro$1.update(n, res)
+  //>     res
+
+@experimental
+@main def Test =
+  val t = new Bar
+  assert(t.fib(3) == 2)
+  assert(t.fib(4) == 3)
